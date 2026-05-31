49e21c832d8a8eab784b70fa4395fbd17fa2b1c9
Esmael <esmael6360@gmail.com>
2026-05-31 12:13:01 +0300

    add comment line to db

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -6,6 +6,7 @@ export const connectDB = async () => {
       console.error("MONGODB_URI is not set in environment");
       throw new Error("Missing MONGODB_URI");
     }
+    // comment line
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("DB Connected");
   } catch (error) {
